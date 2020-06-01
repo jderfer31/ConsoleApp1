cdad2faad165ad7fd461314aa2f7d7c3c22e9512
jderfer0979@gmail.com <jderfer0979@gmail.com>
2020-06-01 18:00:42 -0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string name = "Jeremy";
+            string last = "Langenderfer";
+            string Car = "Honda";
+            string Model = "Accord";
+            Console.WriteLine("Hello my name is {0} {1}", name, last);
+            Console.WriteLine("I drive a {0} {1}", Car, Model);
+        }
+    }
+}
